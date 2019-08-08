244002e8cd096e96afc0c866e6dafaa088d90d1e
negosan <425569885@qq.com>
2019-08-08 15:04:56 +0800

    第一次提交

---
+++ ht/src/main/java/com/neusoft/ht/HtApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.ht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtApplication.class, args);
+	}
+
+}
