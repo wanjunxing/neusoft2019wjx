d68b89472e7f86a052ba4555f12ac1108fc4831a
negosan <425569885@qq.com>
2019-08-08 23:35:39 +0800

    添加供暖缴费模块的mapper接口 by 黄宇德

---
+++ ht/src/main/java/com/neusoft/ht/fee/mapper/IHomeMapper.java
@@ -0,0 +1,16 @@
+package com.neusoft.ht.fee.mapper;
+
+import java.util.List;
+
+import com.neusoft.ht.heating.model.HomeModel;
+
+public interface IHomeMapper {
+	//C
+	public void create(HomeModel homeModel) throws Exception;
+	//U
+	public void update(HomeModel homeModel) throws Exception;
+	//R
+	public List<HomeModel> selectListByAll() throws Exception;
+	//D
+	public void delete(HomeModel homeModel) throws Exception;
+}
