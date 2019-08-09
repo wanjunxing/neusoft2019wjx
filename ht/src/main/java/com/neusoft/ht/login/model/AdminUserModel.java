8bfa6b9fa7c7739a37f2b1eab20d40ee2971a6c8
zhangxiaolong <522196937@qq.com>
2019-08-09 10:14:28 +0800

    登陆模块的model

---
+++ ht/src/main/java/com/neusoft/ht/login/model/AdminUserModel.java
@@ -0,0 +1,11 @@
+package com.neusoft.ht.login.model;
+
+import lombok.Data;
+
+@Data
+public class AdminUserModel {
+
+	private String uUserId = null;
+	private String uPassword = null;
+	private String uName = null;
+}
